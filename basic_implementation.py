306c7fbb4f6f7c4095c02051ebc6065881afe951
Rubab MAlik <malikrubab@gmail.com>
2017-07-07 12:29:02 -0400

    start of basic implementation

--- basic_implementation.py
+++ basic_implementation.py
@@ -1,60 +1,56 @@
 import numpy as np
 from sklearn.datasets import fetch_mldata
 import random
+from sklearn.metrics import mean_squared_error
+
 saved_mnist = '/home/rubab/PycharmProjects/neural_network'
 mnist = fetch_mldata('MNIST original', data_home='saved_mnist')
-import sys
 
-print np.shape(mnist.data)
-print np.unique(mnist.target)
-print
-#
-# # set parameters (learning rate, regularization strength*, randomized initial weights)
-# learning_rate = .01 # .01 is commonly used - NOT IMPORTANT RIGHT NOW
-# weight = np.zeros(10) # use randomly assigned ones (must be variable to avoid getting stuck in local min), what range to use???
-# bias = np.zeros(10) # initialize biases to zero?
-# inputs = np.zeros(10) # use import to incorporate inputs
-#
-# # compute activations from inputs and weights
-# activation =  np.dot(np.transpose(weight),inputs) + bias
-#
-# # pass activations through transfer function
-# apply_transfer = np.tanh(activation)
-#
-# # calculate error/loss
-# error = None
-# gradient = None
-#
-# # adjust parameters based on loss using backpropagation and GD
+
 
 #Forward Propagation - using one hidden layer
 
-subsample = mnist.data[0:5]
-print np.shape(mnist.data)
+subsample = mnist.data[0:5] #use subsample for running and training entire NN
+current_input = mnist.data[0] #use current_input for building NN
 
-current_input = mnist.data[0]
-print np.shape(current_input)
-print current_input
+# originally 784 row array, reshape tp 28x28 array
+current_input = np.reshape(current_input, (28, 28))
+print current_input #looks like a zero
+input_list = []
+input_list.append(current_input)
 
-print
-print np.shape(current_input)
-print current_input
+possible_outputs = list(range(0, 10))
 
-weight = np.random.randint(0, high = 255, size = [10, 784])
-print 'weight'
-print weight
-print np.shape(weight)
-print
-print
 
-# where does number of nodes come into play?
-activation_1 =  np.dot(weight,current_input)
-# print activation_1
 
-apply_transfer_function_1 = np.tanh(activation_1)
-# use as input of next layer
-# print apply_transfer_function_1
+def activation(inputs):
+    activation_1 = 0
+    print 'length of input', len(inputs)
+    for input in range(0, len(inputs)):
+        weight_1 = np.random.randint(0, high = 255, size = [10, 28])
+        activation_1 +=  np.dot(weight_1,inputs[input])
+        #activation_1 is a 10x28 array
 
-activation_2 =  np.dot(np.transpose(weight),apply_transfer_function_1)
-# no transfer function applied to outermost layer, start BP
-# print activation_2
+        apply_transfer_function_1 = np.tanh(activation_1)
+        #apply_transfer_function_1 is a 10x28 array with only 0s and 1s
+
+        weight_2 = np.random.randint(0, high=255, size=[10, 28])
+        activation_2 =  np.dot(np.transpose(weight_2),apply_transfer_function_1)
+        #activation_2 is a 28x28 array (back to original image dimensions)
+
+        linear_classifier = np.sign(activation_2) #thresholding?
+
+output = [random.choice(possible_outputs)]
+expected_output = [0]
+
+def error_computation(output, expected_output):
+   error = mean_squared_error(output, expected_output)
+   print error
+   return error
+
+def gradient_descent():
+    
+
+
+activation(input_list)
+error_computation(output, expected_output)
